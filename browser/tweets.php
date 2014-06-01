a0d1d7d186e8a4fb7b76545554ba625cc4191ada
sebastian-meier <contact@sebastianmeier.eu>
2014-06-01 17:59:53 +0200

    Browser based execution

---
+++ browser/tweets.php
@@ -0,0 +1,8 @@
+<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"></head><body onload="window.setTimeout('window.location.reload()', 2000)">
+<?php
+
+	include("../cron/tweets.php");
+
+?>
+</body>
+</html>
