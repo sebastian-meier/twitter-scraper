1e1dbbfac7b62d8356f50dad628dd76a891e0f62
sebastian-meier <contact@sebastianmeier.eu>
2014-06-01 18:43:21 +0200

    Bugfixes

--- cron/users.php
+++ cron/users.php
@@ -9,7 +9,7 @@ require_once ('../lib/db.php');
 $cb = \Codebird\Codebird::getInstance();
 
 //load user-token and -secret from database
-$sql = 'SELECT `token`, `secret` FROM `twitter_token` WHERE request = "'.$_GET["request"].'" LIMIT 1';
+$sql = 'SELECT `token`, `secret` FROM `'.$db_prefix.'twitter_token` LIMIT 1';
 $result = query_mysql($sql, $link);
 while ($row = mysql_fetch_assoc($result)) {
 	$cb->setToken($row["token"], $row["secret"]);
@@ -17,11 +17,10 @@ while ($row = mysql_fetch_assoc($result)) {
 mysql_free_result($result);
 
 //load request-id, current page and check if we are already done here
-$sql = 'SELECT `id`, `page`, `done`, `pause` FROM `twitter_requests` WHERE city = "'.$_GET["request"].'" LIMIT 1';
+$sql = 'SELECT `id`, `done`, `pause` FROM `'.$db_prefix.'twitter_requests` WHERE request = "'.$_GET["request"].'" LIMIT 1';
 $result = query_mysql($sql, $link);
 while ($row = mysql_fetch_assoc($result)) {
 	$request_id = $row["id"];
-	$since = $row["page"];
 	$done = $row["done"];
 	$pause = $row["pause"];
 }
@@ -37,7 +36,7 @@ if($pause<1){
 		//Create the user-id string
 		$users = "";
 
-		$sql = 'SELECT `id` FROM `'.$city.'_twitter_users` WHERE done = 0 LIMIT 100';
+		$sql = 'SELECT `id` FROM `'.$db_prefix.'twitter_users` WHERE done = 0 AND request_id = '.$request_id.' LIMIT 100';
 		$result = query_mysql($sql, $link);
 		while ($row = mysql_fetch_assoc($result)) {
 			if($users != ""){ $users .= ","; }
@@ -47,15 +46,16 @@ if($pause<1){
 
 		$reply = $cb->users_lookup(array('user_id'=>$users, 'include_entities'=>'true'));
 
-		$g += processResults($reply, $link, $request_id, $city);
+		$g += processResults($reply, $link, $request_id, $_GET["request"]);
 	}
 
-	echo 'users:'.$city.':'.$g;
+	echo 'users:'.$_GET["request"].':'.$g;
 }else{
 	echo 'pause';
 }
 
-function processResults($results, $link, $request_id, $city){
+function processResults($results, $link, $request_id, $request){
+	global $db_prefix;
 	$c = 0;
 
 	foreach ($results as $result) {
@@ -63,12 +63,11 @@ function processResults($results, $link, $request_id, $city){
 	
 			//Store all values of the object as metadata, beside the user-object
 			foreach($result as $key => $value){
-				goDeeper("", $key, $value, $link, $request_id, $city, $result);
+				goDeeper("", $key, $value, $link, $request_id, $request, $result);
 			}
 
-			$sql = 'UPDATE `'.$city.'_twitter_users` SET `done` = 1 WHERE id = '.$result->id_str;
+			$sql = 'UPDATE `'.$db_prefix.'twitter_users` SET `done` = 1 WHERE id = '.$result->id_str;
 			$update = query_mysql($sql, $link);
-			mysql_free_result($update);
 			$c++;
 
 		}
@@ -77,14 +76,14 @@ function processResults($results, $link, $request_id, $city){
 	return $c;
 }
 
-function goDeeper($parent, $key, $value, $link, $request_id, $city, $result){
+function goDeeper($parent, $key, $value, $link, $request_id, $request, $result){
+	global $db_prefix;
 	if(!is_object($value) && !is_array($value) && $key != "status"){
-		$sql = 'INSERT INTO `'.$city.'_twitter_usermetadata` (`twitter_user_id`, `field_key`, `field_value`)VALUES("'.$result->id_str.'", "'.$parent.$key.'", "'.str_replace('"', "'", $value).' ")';
+		$sql = 'INSERT INTO `'.$db_prefix.'twitter_usermetadata` (`twitter_user_id`, `field_key`, `field_value`, `request_id`)VALUES("'.$result->id_str.'", "'.$parent.$key.'", "'.str_replace('"', "'", $value).'", '.$request_id.')';
 		$insert = query_mysql($sql, $link);
-		mysql_free_result($insert);
 	}else if((is_object($value) || is_array($value)) && $key != "status"){
 		foreach ($value as $inner_key => $inner_value) {
-			goDeeper($key." ", $inner_key, $inner_value, $link, $request_id, $city, $result);
+			goDeeper($key." ", $inner_key, $inner_value, $link, $request_id, $request, $result);
 		}
 	}
 }
